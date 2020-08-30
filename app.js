6db71aab02640148106dbc9100d0e573798a3d8c
Vinaybhushana Reddy <vinay@caratred.com>
2020-08-30 11:56:39 +0530

    initial commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var allRoutes = require('./routes/index.route');
+import cors from 'cors';
 
 
 var app = express();
